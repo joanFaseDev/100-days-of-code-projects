9f3322f13c382ad0b4ed0992f3d8aca61fa1b1ff
joan_fase <jordanfalaise.dev@gmail.com>
2023-04-05 13:06:36 +0200

    Added color system switch functions, logic to change font color based on overall background's color intensity & proper styling of the document

--- 1.ColorFlipper/README.md
+++ 1.ColorFlipper/README.md
@@ -6,9 +6,6 @@
 This is the first project of my '100 days of code' challenge. For this challenge, i want to focus less on tutorials and more on actual projects where i'm not guided and have to think / find solutions by myself. Didn't want to set the bar too high for my first day so i picked a project that seems manageable with a language i'm familiar with.
 
 ### To Do
-- Solve a problem where the span text content is sometimes equal to 'undefined' (use console.log())
-- Define a default background color
-- Properly style the whole application
 
 ### Done
 - Access the body / button element through querySelector()
@@ -16,9 +13,17 @@ This is the first project of my '100 days of code' challenge. For this challenge
 - Found a GitHub repo with all css named colors (plus their hex values) packed in an object 
 - Create two functions: one to randomly generate an integer between a minimum and maximum value, the other to randomly generate one of the 148th css named color's name
 - Access the span element nested inside the heading element and changed its rendered text into the name of the new background color
+- Implement the logic to switch between named colors and hex ones
+- Initialize a random background color every time the document is loaded / refreshed
+- Implement the logic to change the font color depending of the overall intensity of the actual background color
+- Implement the logic to change the color/background color of the buttons depending of body's background color
+- Style the document
 
 ## Sources
 - https://www.freecodecamp.org/news/javascript-projects-for-beginners/#how-to-create-a-color-flipper
 - https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model/Introduction
 - https://gist.github.com/bobspace/2712980
 - https://javascript.info/modules-intro
+- https://stackoverflow.com/questions/3942878/how-to-decide-font-color-in-white-or-black-depending-on-background-color
+- https://www.rapidtables.com/convert/color/how-hex-to-rgb.html
+- https://easings.net/
--- 1.ColorFlipper/script.js
+++ 1.ColorFlipper/script.js
@@ -2,48 +2,85 @@ import { CSS_COLOR_NAMES } from "./css-colors.js";
 
 const clickBtn = document.querySelector('#click');
 const colorSystemBtn = document.querySelector('#hex-color');
-
 const body = document.querySelector('body');
 const span = document.querySelector('span');
+const btns = document.querySelectorAll('button');
+
+let colorSystem = 'named color';
+
+initBackgroundColor();
 
 clickBtn.addEventListener('click', (evt) => {
-    const randomColor = pickRandomColorName();
+    const randomColor = pickRandomColor(colorSystem);
+    changeFontColor(randomColor);
     body.style.backgroundColor = randomColor;
     span.innerText = randomColor;
 });
 
+// Switch the actual color system (named or hex) and its button's label. Update the background's color value. 
 colorSystemBtn.addEventListener('click', (evt) => {
-    const colorSystem = colorSystemBtn.textContent;
-    const colorValue = span.innerText;
-
-    switch (colorSystem) 
-    {
-        case 'Hexadecimal Color':
-            const hexColor = CSS_COLOR_NAMES[colorValue];  
-            span.innerText = hexColor;
+    let actualColor = span.innerText;
+    switch (colorSystem) {
+        case 'named color':
+            span.innerText = CSS_COLOR_NAMES[actualColor];
+            colorSystem = 'hexadecimal color';
             colorSystemBtn.textContent = 'Named Color';
             break;
     
-        case 'Named Color':
-            const namedColor = Object.keys(CSS_COLOR_NAMES).find((key) => CSS_COLOR_NAMES[key] === colorValue);
-            span.innerText = namedColor;
+        default:
+            span.innerText = Object.keys(CSS_COLOR_NAMES).find((key) => CSS_COLOR_NAMES[key] === actualColor);
+            colorSystem = 'named color';
             colorSystemBtn.textContent = 'Hexadecimal Color';
             break;
     }
 });
 
-function pickRandomColorName()
+// Depending of the color system selected, randomly pick a color from an array of named colors or hexadecimal colors
+function pickRandomColor(colorSystem)
 {
-    const colorsName = Object.keys(CSS_COLOR_NAMES);
+    const colorsName = (colorSystem === 'named color') ? Object.keys(CSS_COLOR_NAMES) : Object.values(CSS_COLOR_NAMES);
     const indexMin = 0;
-    const indexMax = colorsName.length - 1;
+    const indexMax = colorsName.length;
     const randomIndex = getRandomInt(indexMin, indexMax);
     return colorsName[randomIndex];
 }
 
+// Get a random integer between a minimum (inclusive) and maximum (exclusive) value
 function getRandomInt(minValue, maxValue)
 {
     const min = Math.ceil(minValue);
     const max = Math.floor(maxValue);
     return Math.floor(Math.random() * (max - min) + min);
+}
+
+// Choose a random background color every time the document is loaded
+function initBackgroundColor()
+{
+    const randomColor = pickRandomColor(colorSystem);
+    changeFontColor(randomColor);
+    body.style.backgroundColor = randomColor;
+    span.innerText = randomColor;
+}
+
+// Adapt the font color (black or white) to the overall intensity of the background color
+function changeFontColor(color)
+{
+    const hexColor = (colorSystem === 'hexadecimal color') ? color : CSS_COLOR_NAMES[color];
+    // Convert hexadecimal value into rgb ones by parsing them in base 16
+    const hexRed = hexColor.slice(1, 3);
+    const hexGreen = hexColor.slice(3, 5);
+    const hexBlue = hexColor.slice(5);
+
+    const red = parseInt(hexRed, 16);
+    const green = parseInt(hexGreen, 16);
+    const blue = parseInt(hexBlue, 16);
+
+    // Convert rgb values into grayscale ones then compute a weighted average of the red, green and blue values to get the overall intensity
+    const overallIntensity = Math.round(red * 0.299 + green * 0.587 + blue * 0.114);
+
+    body.style.color = (overallIntensity > 186) ? 'black' : 'white';
+    btns.forEach((btn) => {
+        btn.style.backgroundColor = body.style.color;
+        btn.style.color = (btn.style.backgroundColor === 'black') ? 'white' : 'black';
+    });
 }
