ebe51d89091c1a58019904e2ae749edf9e97d88f
joan_fase <jordanfalaise.dev@gmail.com>
2023-04-07 15:10:13 +0200

    Day 3 | Palindrome Checker: Added logic to check palindrome, generate random palindrome, check user input && render result to the screen. JavaScript part completed, CSS to follow.

---
+++ 3.PalindromeChecker/README.md
@@ -0,0 +1,27 @@
+# Palindrome Checker
+
+## Goals
+
+To build a vanilla JavaScript application taking a user input and checking if that input is indeed a palindrome, that is to say 'a word, phrase, or sequence that reads the same backwards as forwards' (e.g. madam).
+
+### To Do
+    - Through JavaScript, create a paragraph element to nest inside the output of the function.
+    - Style the paragraph with a different color depending of the input being a palindrome or not.
+
+### Done
+    - Create an index.html file with a heading, a figure for the palindrome definition (for those who doesn't know what it means), a label/input text element and one button to send the data. Also a footer with name and year.
+    - Add a note to warn users that punctuation, capitalization and spaces are ignored.
+    - Add a button and logic to randomly generate a true palindrome (for those who are curious, lazy or didn't quite get the meaning of the word)
+    - Prevent the form from being sent when the user clicks on the form's button
+    - A function that takes the user's input, convert it into an array, create an other array based on the first but with all elements reversed then check if the arrays content match and return the result.
+
+
+
+## Sources
+    - https://www.w3.org/TR/2011/WD-html5-20110525/sections.html
+    - https://developer.mozilla.org/en-US/docs/Web/HTML/Element/blockquote
+    - https://developer.mozilla.org/en-US/docs/Web/HTML/Element/figure
+    - https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
+    - https://becomeawritertoday.com/palindrome-words/
+    - http://www.palindromelist.net/
+    - https://developer.mozilla.org/en-US/docs/Web/HTML/Element/details
---
+++ 3.PalindromeChecker/script.js
@@ -0,0 +1,159 @@
+const btnCheck = document.querySelector('#btn-check');
+const btnGenerate = document.querySelector('#btn-generate');
+const label = document.querySelector('#label-input')
+const input = document.querySelector('#user-input');
+const mainCtn = document.querySelector('#main-container');
+
+const palindromes = [
+    'Abba',
+    'Aibohphobia',
+    'Bib',
+    'Bob',
+    'Civic',
+    'Deified',
+    'Detartrated',
+    'Dewed',
+    'Eve',
+    'Hannah',
+    'Kayak',
+    'Level',
+    'Madam',
+    'Malayalam',
+    'Minim',
+    'Mom', 
+    'Murdrum',
+    'Noon',
+    'Nun',
+    'Otto',
+    'Peep',
+    'Pop',
+    'Racecar',
+    'Radar',
+    'Redder',
+    'Refer',
+    'Repaper',
+    'Rotator',
+    'Rotavator',
+    'Sagas',
+    'Sis',
+    'Solos',
+    'Stats',
+    'Tattarrattat',
+    'Tenet', 
+    'Wow',
+    'Taco cat',
+    'Never odd or even',
+    'Madam, I’m Adam',
+    'Mr. Owl ate my metal worm.',
+    'Was it a car or a cat I saw?',
+    'Murder for a jar of red rum.',
+    'Go hang a salami, I’m a lasagna hog.',
+    'Do geese see God?',
+    'Evil olive',
+    'Amore, Roma',
+    'A man, a plan, a canal: Panama',
+    'My gym',
+    'No lemon, no melon',
+    'Top spot',
+    'Sit on a potato pan, Otis!',
+    'Ah. Satan sees Natasha.',
+    'Cigar? Toss it in a can. It is so tragic.',
+    'Did Hannah See bees? Hannah did.',
+    'Borrow, or rob?',
+    'Eva, can I see bees in a cave?',
+    'Poor Dan is in a droop.',
+    'Oozy rat in a sanitary zoo.',
+    'Step on no pets.',
+    'Able was I ere I saw Elba.',
+    'Sir, I demand, I am a maid named Iris.',
+    'We panic in a pew.',
+    'Won’t lovers revolt now?',
+    'Don’t nod.'
+];
+
+// BUTTON CLEAR
+btnCheck.addEventListener('click', (evt) => {
+    // Prevent the form's data being submitted to the server
+    evt.preventDefault();
+    // Clear the DOM of the previous answer
+    removeResult();
+
+    const str = input.value;
+    if (checkValidInput(str))
+    {
+        label.textContent = 'Write something here';
+        const isPalindrome = checkPalindrome(str);
+        generateResult(str, isPalindrome);
+        input.value = '';
+    }
+    else
+    {
+        label.textContent = 'Write something here WITH AT LEAST ONE CHARACTER!';
+    }
+});
+
+// BUTTON GENERATE
+btnGenerate.addEventListener('click', (evt) => {
+    generatePalindrome();
+});
+
+// Delete the result message from the DOM
+function removeResult()
+{
+    const result = document.querySelector('.result');
+    if (result) result.remove();
+}
+
+// Display to the screen the result of user's input being a palindrome or not
+function generateResult(inputTested, result)
+{
+    const paragraph = document.createElement('p');
+    paragraph.setAttribute('class', 'result');
+    paragraph.textContent = (result) ? `"${inputTested}" is a valid palindrome.`
+        : `"${inputTested}" is not a palindrome.`;
+    mainCtn.append(paragraph);
+}
+
+// Generate a random palindrome and display it in the input field
+function generatePalindrome()
+{
+    const max = palindromes.length;
+    const randomIdx = randomInteger(0, max);
+    const palindrome = palindromes[randomIdx];
+    input.value = palindrome;
+}
+
+// Generate a random integer between min and max values
+function randomInteger(min, max)
+{
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+// Check if input string is a palindrome or not
+function checkPalindrome(input)
+{
+    // Regular expression matching any punctuation characters (litteral notation is used for better performances)
+    const regEx = /[!"#$%&'’()*+,-./:;<=>?@[\]^_`{|}\s~]/g;
+    const noPunctuation = input.replace(regEx, '');
+    console.log(noPunctuation);
+    // Convert into an array and create a copy to use the reverse method then convert back into string
+    const chars = Array.from(noPunctuation, (char) => char.toLowerCase()).join('');
+    const reverseChars = Array.from(chars).reverse().join('');
+
+    console.log(chars, reverseChars);
+
+    // If both array's contents match then the user's input is a palindrome
+    return chars === reverseChars;
+}
+
+// Test checkPalindrome() logic on a small palindrome sample
+function checkPalindromeFunction()
+{
+    return palindromes.filter((elem) => !checkPalindrome(elem));
+}
+
+// Check if th user's input is neither a null object nor it is an empty string
+function checkValidInput(input)
+{
+    return typeof input !== null && input.trim().length !== 0
+}
