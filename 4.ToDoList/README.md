edc6d7f91281f7a5f9e85b049066faf18abf067d
joan_fase <jordanfalaise.dev@gmail.com>
2023-04-09 11:59:36 +0200

    Day 5 | To do List: Added logic to add an entry, clear or remove this entry, check for user's data validity, check for list length and create/remove one if needed

---
+++ 4.ToDoList/README.md
@@ -0,0 +1,37 @@
+# To do List
+
+## Goals
+
+To build a vanilla JavaScript application where a user can submit an input (a task to do for the day) at a time, each input is then add to the screen with two buttons associated, one for clearing the task (the task is done) one for removing it entirely (the task is cancelled or the user is lazy). A nice touch will be to include a feature where the user can drag and drop each entry to sort them from 'important' to 'can be done later'.
+
+### To Do
+
+
+        BONUS:
+        - A different background color / color depending on the entry's position in the list being even or odd.
+        - Drag and drop logic
+
+### Done
+
+    - Create an index.html file with a heading (title), a form (for the input & button, no label) and a footer (year and name). Heading and form should be nested inside a div to facilitate the layout later.
+    - Create a script.js file with:
+        - a reference to the input and button element
+        - an array of a dozen of random tasks to be generating for testing purpose
+        - a function retrieving the user's input data, taking that data and outputting it in a list element. The list element should be created when at least one entry is added and should be removed when there's no more entry in the list. Each user data should be nested inside a paragraph element like this:
+        ```
+        <li>
+            <div>
+                <p>User Input</p>
+                <button>Clear (V)</button>
+                <button>Remove (X)</button>
+            </div>
+        </li>
+        ```
+        - a callback function on each button to clear / remove the data associated with it. A user data 'cleared' would have a different style apply to it. The clear button, once clicked, have its label change into 'unclear'. Clicking on the button then have the effet of removing the style. 
+        - Add logic to check for 'invalid' input and, if needed, warn users about it.
+
+## Sources
+
+    - https://www.w3.org/TR/2011/WD-html-markup-20110113/ul.html
+    - https://developer.mozilla.org/en-US/docs/Web/API/Element/setAttribute
+    - https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API
---
+++ 4.ToDoList/script.js
@@ -0,0 +1,162 @@
+const input = document.querySelector('#user-data');
+const btnAdd = document.querySelector('#btn-add');
+const btnGenerate = document.querySelector('#btn-generate');
+const main = document.querySelector('#main-content');
+
+console.dir(main);
+
+btnAdd.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    clearWarningMessage();
+    generateTask();
+    clearField();
+});
+
+btnGenerate.addEventListener('click', (evt) => {
+    const randomTask = getRandomTask();
+    input.value = randomTask;
+});
+
+// Create all DOM elements necessary to render the new list entry on screen
+function generateTask()
+{
+    const data = input.value;
+    const isValid = checkUserInput(data);
+
+    if (isValid) 
+    {
+        const li = document.createElement('li');
+        const div = document.createElement('div');
+        const paragraph = document.createElement('p');
+        const btnClear = document.createElement('button');
+        const btnRemove = document.createElement('button');
+        
+        paragraph.textContent = data;
+    
+        btnClear.setAttribute('type', 'button');
+        btnClear.textContent = 'Clear';
+        btnRemove.setAttribute('type', 'button');
+        btnRemove.textContent = 'Delete';
+        
+        // Add / remove special style to completed tasks
+        btnClear.addEventListener('click', (evt) => {
+            if (btnClear.textContent === 'Clear')
+            {
+                paragraph.setAttribute('class', 'is-clear');
+                btnClear.textContent = 'Unclear';
+            }
+            else
+            {
+                paragraph.removeAttribute('class');
+                btnClear.textContent = 'Clear';
+            }
+        });
+        
+        // Remove the list item if its designated remove button is clicked
+        btnRemove.addEventListener('click', (evt) => {
+            li.remove();
+            removeList();
+        });
+    
+        let ul = checkList();
+        if (!ul)
+        {
+            ul = document.createElement('ul');
+            ul.setAttribute('id', 'tasks-list'); 
+        }
+    
+        div.append(paragraph);
+        div.append(btnClear);
+        div.append(btnRemove);
+        li.append(div);
+        ul.append(li);
+        main.append(ul);
+    }
+}
+
+// Clear the input field's content
+function clearField()
+{
+    input.value = '';
+}
+
+// Remove the list if there's no more entries inside
+function removeList()
+{
+    const ul = checkList();
+    if (ul)
+    {
+        if (ul.children.length === 0)
+        {
+            ul.remove();
+        }
+    }
+}
+
+// Check if the task list has already been created
+function checkList()
+{
+    const ul = document.getElementById('tasks-list');
+    return ul;
+}
+
+// Check user's input validity
+function checkUserInput(data)
+{
+    if (data.length <= 4)
+    {
+        const paragraph = document.createElement('p');
+        paragraph.textContent = 'Valid entry should be AT LEAST 5 characters long.';
+        paragraph.setAttribute('id', 'warning');
+        main.append(paragraph);
+        return false;
+    }
+
+    return true;
+}
+
+// Delete warning message if there's already one
+function clearWarningMessage()
+{
+    const warning = document.getElementById('warning');
+    if (warning)
+    {
+        warning.remove();
+    }
+}
+
+// Return a random to-do task
+function getRandomTask()
+{
+    const tasks = [
+        'Play an instrument',
+        'Write a short story',
+        'Do a deep dive on a subject that interests you',
+        'Fill out a crossword puzzle',
+        'Watch a rom-com marathon',
+        'Plan your next getaway',
+        'Learn a how to do new hairstyle',
+        'Try a coloring app',
+        'Play Elden Ring. Defeat Malenia.',
+        'Whip up a new recipe',
+        'Indulge in a sundae bar',
+        'Build a gingerbread house',
+        'Write to a deployed soldier',
+        'Put together a care package',
+        'Work on your wish list',
+        'Unplug your devices',
+        'Paint your nails',
+        'Take a bubble bath',
+        'Walk down memory lane',
+        'Give a pep talk',
+        'Watch a movie outdoors'
+    ];
+    const randomIdx = getRandomInteger(0, tasks.length);
+    return tasks[randomIdx];
+}
+
+// Return a random integer between min (inclusive) and max (exclusive)
+function getRandomInteger(min, max)
+{
+    return Math.floor(Math.random() * (max - min) + min);
+}
