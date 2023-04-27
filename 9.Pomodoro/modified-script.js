46ebc78a40800677d51a9236d467b15e51471857
joan_fase <jordanfalaise.dev@gmail.com>
2023-04-27 18:48:00 +0200

    Added logic and style on the START/PAUSE button plus change of mode/style when a countdown end. New code version is now fully updated and the application, while missing some 'juicy' features, is functionnal.

--- 9.Pomodoro/modified-script.js
+++ 9.Pomodoro/modified-script.js
@@ -4,6 +4,7 @@ const body = document.querySelector('body');
 const countdownCtn = document.querySelector('#countdown-ctn');
 const countdownBtns = document.querySelectorAll('.state-btn');
 const startBtn = document.querySelector('#start-btn');
+const pomodoroUser = document.querySelector('#pomodoro-number');
 
 // Get the spans elements which display minutes and seconds
 const minutes = document.querySelector('#minutes');
@@ -20,29 +21,48 @@ let countdown;
 // Actual id of the setInterval method
 let idInterval;
 
+// Number of 'pomodoro' (1 pomodoro = 25 minutes of work) the user earned
+let pomodoroCount = 0;
+
 const MILLISECOND = 1000;
 
 // 25 minutes converted into milliseconds
-const POMODORO_TIME = 1500000;
+// const POMODORO_TIME = 1500000;
+const POMODORO_TIME = 15000;
 
 // 5 minutes converted into milliseconds
-const SHORT_BREAK_TIME = 300000;
+// const SHORT_BREAK_TIME = 300000;
+const SHORT_BREAK_TIME = 5000;
 
 // 15 minutes converted into milliseconds
-const LONG_BREAK_TIME = 900000;
+// const LONG_BREAK_TIME = 900000;
+const LONG_BREAK_TIME = 10000;
 
 const config = {
     'pomodoro': {
         countdown: POMODORO_TIME,
-        message: 'Time to work!'
+        message: 'Time to work!',
+        startButton: {
+            state: 'pause',
+            textContent: 'START'
+            
+        }
     },
     'short-break': {
         countdown: SHORT_BREAK_TIME,
-        message: 'Time for a short break!'
+        message: 'Time for a short break!',
+        startButton: {
+            state: 'pause',
+            textContent: 'START'
+        }
     },
     'long-break': {
         countdown: LONG_BREAK_TIME,
-        message: 'Time for a long break!'
+        message: 'Time for a long break!',
+        startButton: {
+            state: 'pause',
+            textContent: 'START'
+        }
     }
 };
 
@@ -50,23 +70,46 @@ updateMode('pomodoro');
 
 countdownBtns.forEach((btn) => {
     btn.addEventListener('click', (event) => {
-        const mode = event.target.dataset['state'];
+        const currentMode = event.target.dataset['state'];
+        mode = currentMode;
         updateMode(mode);
     });
 });
 
 startBtn.addEventListener('click', (event) => {
-    // Call the functions once, out of setInterval loop, to make up for the first delay
-    updateCountdown(countdown, minutes, seconds);
-    countdown = passTime(countdown, MILLISECOND);
 
-    const id = setInterval(() => {
+    // If the application WAS in pause BEFORE pressing the button
+    if (checkPause())
+    {
+        // Call the functions once, out of setInterval loop, to make up for the first delay
         updateCountdown(countdown, minutes, seconds);
         countdown = passTime(countdown, MILLISECOND);
-        checkUserClick(id);
-    }, MILLISECOND);
+    
+        const id = setInterval(() => {
+            // Countdown is updated visually then decreased then updated and so forth and so on
+            updateCountdown(countdown, minutes, seconds);
+            countdown = passTime(countdown, MILLISECOND);
+    
+            // Prevent the countdown to keep decreasing when a change of mode is required
+            checkUserClick(id);
 
-    idInterval = updateIdInterval(id);
+            // Check if countdown is over and if it is, handle the mode update
+            checkCountdown(id);
+        }, MILLISECOND);
+    
+        idInterval = updateIdInterval(id);
+        togglePause();
+    }
+    // If the application WAS NOT in pause BEFORE pressing the button
+    else
+    {
+        clearInterval(idInterval);
+        console.log('this is coming from the else block!');
+        togglePause();
+    }
+
+
+
 });
 
 // Update the behavior and style of the application
@@ -97,6 +140,8 @@ function updateConfig(mode)
 {
     countdown = config[mode].countdown;
     message.textContent = config[mode].message;
+    startBtn.dataset['state'] = config[mode].startButton.state;
+    startBtn.textContent = config[mode].startButton.textContent;
 }
 
 // Update the current theme of the application based on the actual mode
@@ -137,6 +182,7 @@ function updateIdInterval(id)
     return id;
 }
 
+// If user requier a change of mode, the interval is automatically cleared
 function checkUserClick(id)
 {
     countdownBtns.forEach((btn) => {
@@ -145,4 +191,55 @@ function checkUserClick(id)
             clearInterval(id);
         });
     });
+}
+
+// Check if the application is in pause or not
+function checkPause()
+{
+    const state = startBtn.dataset['state'];
+    return state === 'pause';
+}
+
+// Switch the application in pause/start mode depending of what is current mode is
+function togglePause()
+{
+    startBtn.textContent = (startBtn.dataset['state'] === 'pause') ? 'PAUSE' : 'START';
+    startBtn.dataset['state'] = (startBtn.dataset['state'] === 'pause') ? 'start' : 'pause';
+}
+
+// Increment the user's pomodoro by 1;
+function incrementPomodoro()
+{
+    pomodoroCount = pomodoroCount + 1;
+    pomodoroUser.textContent = `#${pomodoroCount}`;
+    return pomodoroCount;
+}
+
+// Check if the countdown is over and, if it is, handle the mode's update 
+function checkCountdown(id)
+{
+    if (countdown < 0)
+    {
+        const currentMode = mode
+        if (currentMode === 'pomodoro')
+        {
+            if ((incrementPomodoro()) % 4)
+            {
+                mode = 'short-break';
+            }
+            else
+            {
+                mode = 'long-break';
+            }
+        }
+        else
+        {
+            mode = "pomodoro";
+        }
+        console.log(`${currentMode} into ${mode}`);
+        idInterval = updateIdInterval(id);
+        clearInterval(idInterval);
+        updateMode(mode);
+    }
+    
 }
