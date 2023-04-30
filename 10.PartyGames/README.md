f6d667c9da2493a6bbfd2b0bca573dd3b4325cf6
joan_fase <jordanfalaise.dev@gmail.com>
2023-04-30 09:59:38 +0200

    Refactored the current Hangman game implementation. Introduced myself to the Canvas API with the end goal of drawing a proper gallows and its hangman.

--- 10.PartyGames/README.md
+++ 10.PartyGames/README.md
@@ -5,15 +5,35 @@
 - [X] Find an API to generate random word
 - [X] Connect to the API and retrieve a random word
 - [X] Create as many colored divs as there are letters in the random word received.
-- [] Refactor the existing code before adding new features to the application.
-- [] Display a hangman's drawing on screen (using canvas?)
+- [X] Refactor the existing code before adding new features to the application.
+- [] Display a hangman's drawing on screen (using canvas?):
+    - [X] Introduction to the Canvas API.
+    - [X] Learn how to properly set up a canvas element and its context.
+    - [X] Learn how the canvas' coordinates system works.
+    - [X] Experiment with the rectangle functions.
+    - [X] Using rectangles, draw satisfying gallows.
+    - [] Experiment with the path functions.
+- [] Make the drawing half transparent on screen. Each element of the drawing can become fully opaque when activated.
 - [] Ask the user for a letter and get its input.
 - [] If the letter is correct, display the letter in its div.
 - [] If the letter is not correct, show a part of the hangman.
 - [] The game ends when all letters are found or when the drawing is complete.
 - [] Victory goes to the player who have scored the most points.
 
+## Worthy of note
+- **<canvas>** is an HTML element that can be used to draw graphics via scripting.
+- Default size of the canvas element is **300px x 150px** (width * height).
+- To avoid distortion, it is recommended to set custom width and height for the canvas through its HTML attributes instead of using CSS.
+- To draw graphics on the canvas, a JavaScript **context** object is used.
+- Origin of the canvas gris is set to the **top left corner** at coordinate (0, 0).
+- There are three functions that draw rectangles on the canvas:
+    * [fillRect - MDN](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/fillRect)
+    * [strokeRect - MDN](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/strokeRect)
+    * [clearRect - MDN](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/clearRect)
+
 ## Links
 * [Getting started with AJAX - MDN](https://developer.mozilla.org/en-US/docs/Web/Guide/AJAX/Getting_Started)
 * [Random Word API](http://random-word-api.herokuapp.com/home)
 * [Free Random Word Generator API - Rando](https://random-word-api.vercel.app/)
+* [Canvas API - MDN](https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API)
+* [Canvas Tutorial - MDN](https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial)
--- 10.PartyGames/hangman.js
+++ 10.PartyGames/hangman.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /**
  * Instances of XMLHttpRequest can communicate with servers.
  * It can send and receive information in various formats (JSON, XML, HTML, ...)
@@ -6,38 +8,17 @@
  * update the page without having to refresh it.
  */
 
-document.querySelector('#requestBtn').addEventListener('click', (event) => {
-    requestHandler();
-    document.querySelector('#requestBtn').setAttribute('disabled', '');
-});
+initHangmanGame();
 
-
-// Handle the request
-function responseHandler(xmlHttpRequest)
+function initHangmanGame()
 {
-    try {
-        if (xmlHttpRequest.target.readyState === XMLHttpRequest.DONE)
-        {
-            if (xmlHttpRequest.target.status === 200)
-            {
-                document.querySelector('#word').textContent = JSON.parse(xmlHttpRequest.target.response);
-                createHangmanSlots(JSON.parse(xmlHttpRequest.target.response).join(''));
-            }
-            else
-            {
-                console.log(`There was a problem with the request.\nRequest status: ${xmlHttpRequest.target.status}`);
-            }
-        }
-        else
-        {
-            console.log(`The response is not ready yet.\nCurrent state is ${xmlHttpRequest.target.readyState}`);
-        }    
-    } 
-    catch (error) 
-    {
-        console.log(`Caught exception: ${error.description}`);
-    }
-    
+    // Wait for the page to be fully loaded before executing the script
+    addEventListener('load', (event) => {
+        const btn = document.querySelector('#requestBtn');
+        btn.addEventListener('click', (event) => {
+            requestHandler();
+        });
+    });
 }
 
 function createRequestObj(method, url)
@@ -49,6 +30,7 @@ function createRequestObj(method, url)
     };
 }
 
+// Configure the request and set the function tasked with handling the response
 function requestHandler()
 {
     const httpRequest = new XMLHttpRequest();
@@ -70,19 +52,149 @@ function requestHandler()
     httpRequest.send();
 }
 
-function createHangmanSlots(word)
+// Check the response's status. If it is ok, set up the Hangman's gameplay loop.
+function responseHandler(xmlHttpRequest)
 {
+    try {
+        if (xmlHttpRequest.target.readyState === XMLHttpRequest.DONE)
+        {
+            if (xmlHttpRequest.target.status === 200)
+            {
+                displayRandomWord(xmlHttpRequest);
+                createHangmanSlots(xmlHttpRequest);
+                deactivateHangmanButton();
+                showHangman();
+            }
+            else
+            {
+                console.log(`There was a problem with the request.\nRequest status: ${xmlHttpRequest.target.status}`);
+            }
+        }
+        else
+        {
+            console.log(`The response is not ready yet.\nCurrent state is ${xmlHttpRequest.target.readyState}`);
+        }    
+    } 
+    catch (error) 
+    {
+        console.log(`Caught exception: ${error.description}`);
+    }
+    
+}
+
+function displayRandomWord(xmlHttpRequest)
+{
+    const randomWord = JSON.parse(xmlHttpRequest.target.response);
+    const p = document.querySelector('#word');
+    p.textContent = randomWord;
+}
+
+function createHangmanSlots(xmlHttpRequest)
+{
+    const randomWord = JSON.parse(xmlHttpRequest.target.response)[0];
     const divParent = document.createElement('div');
     divParent.setAttribute('id', 'letters-ctn');
-    console.log(word);
 
-    for (let i = 0; i < word.length; i++)
+    for (let i = 0; i < randomWord.length; i++)
     {
         const div = document.createElement('div');
         div.setAttribute('class', 'letters');
-        div.textContent = word[i];
+        div.textContent = randomWord[i];
         divParent.append(div);
     }
 
     document.querySelector('#main').append(divParent);
+}
+
+function deactivateHangmanButton()
+{
+    const btn = document.querySelector('#requestBtn');
+    btn.setAttribute('disabled', '');
+}
+
+function showHangman()
+{
+    const canvas = setUpCanvas();
+    const ctx = setUpContext(canvas);
+    drawExample(canvas, ctx);
+    // drawHangman(canvas, ctx);
+}
+
+function setUpCanvas()
+{
+    const main = document.querySelector('#main');
+    const canvas = document.createElement('canvas');
+    canvas.setAttribute('id', 'canvas');
+    canvas.setAttribute('width', 500);
+    canvas.setAttribute('height', 500);
+    canvas.textContent = 'Drawing of a hangman whose body parts get highlighted every time a player make a mistake.';
+    main.append(canvas);
+    return canvas;
+}
+
+function setUpContext(canvas)
+{
+    if (canvas.getContext)
+    {
+        const ctx = canvas.getContext('2d');
+        return ctx;
+    }
+    else
+    {
+        // Fallback content for browsers which do no support the canvas HTML element
+        console.log('There is no support for the canvas element on this browser.');
+    }
+}
+
+function drawHangman(canvas, context)
+{
+    const FULLCOLOR = 'rgba(0, 8, 20, 1)';
+    const HALFCOLOR = 'rgba(0, 8, 20, .5)';
+    const WIDTH = canvas.width;
+    const HEIGHT = canvas.heigth;
+    const HALF_WIDTH = WIDTH / 2;
+    const HALF_HEIGHT = HEIGHT / 2;
+
+    context.fillStyle = HALFCOLOR;
+    context.fillRect(175, 400, 175, 20);
+}
+
+function drawExample(canvas, context)
+{
+    const PRIMARY = 'rgba(247, 37, 133)';
+    const SECONDARY = 'rgba(76, 201, 240)';
+    const HALF_PRIMARY = 'rgba(247, 37, 133, .5)';
+    const HALF_SECONDARY = 'rgba(76, 201, 240, .5)';
+
+    // Example 1
+    context.fillStyle = HALF_PRIMARY;
+    context.fillRect(50, 50, 100, 100);
+    context.fillStyle = HALF_SECONDARY;
+    context.fillRect(75, 75, 100, 100);
+
+    // Example 2
+    // Horizontal Base
+    context.fillStyle = PRIMARY;
+    context.fillRect(150, 440 , 200, 40);
+    context.clearRect(170, 440, 160, 10);
+
+    context.fillStyle = SECONDARY;
+    context.strokeRect(170, 455, 20, 15);
+    context.strokeRect(310, 455, 20, 15);
+
+    // Vertical Base
+    context.fillStyle = PRIMARY;
+    context.fillRect(240, 100, 20, 350);
+
+    context.fillStyle = SECONDARY;
+    context.strokeRect(240, 120, 20, 15);
+    context.strokeRect(240, 155, 20, 15);
+
+    // Horizontal Arm
+    context.fillStyle = PRIMARY;
+    context.fillRect(260, 135, 110, 20);
+
+    // Vertical Arm
+    context.fillRect(350, 155, 10, 45);
+
 }
