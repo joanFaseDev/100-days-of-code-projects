2a6f3144f205db5b03bd52125844a8d0c7bd86d7
joan_fase <jordanfalaise.dev@gmail.com>
2023-05-11 18:51:47 +0200

    Implemented the system keys logic (git, twitter, close application and the all clear feature). Wrote user case where no previous key was registered. Started experimenting with a way to handle the calculator's computation with three properties: leftOperand, rightOperand and operator.

--- 11.Calculator/README.md
+++ 11.Calculator/README.md
@@ -5,14 +5,20 @@
 - [X] Create a Javascript object representing the calculator and another representing the screen. Screen have the responsability to update its content through its method. The calculator instance can then use its screen property to call its screen's instance method. Screen have the responsability but Calculator decide the moment.
 - [X] By default, calculator displays the number ==0==.
 - [X] Create an object Keys representing all calculator's keys. Keys are sorted in three big types: **digits** (all digit keys plus the dot/comma), **operators** (plus, minus, multiply, etc) and **other** (AC and CS).
-
-
-
 - [X] For each key of the calculator, create a key object with its DOM element, its data-key attribute and its data-type attribute.
 - [X] Implement a way to track the last key pressed by a User. Create a previousKey property assigned to an object with all previous key related properties / methods.
 - [X] Create a total property like the previousKey one. The idea is the same, gather all total's properties and methods in one place.
+
+- [X] Implement the logic to all system's keys (git, twitter, close && allClear).
+- [X] Wrote all user's cases where no previous key was registered.
+- [X] Wrote the user case where the previous key and the current one are both digits.
+- [X] Create a computation property for the Calculator object. Use it to store a left and right operand plus one operator which are the basic elements needed to perform any mathematical operation in the Calculator.
+- [X] Limit the number of digits the calculator's screen can display at once (between 14 and 16 seems reasonable for this project).
+
+
+- [] Create a calculate method for the computation object. The method's behavior should change based on three parameters: leftOperand, rightOperand and operator.
+- [] Solve the pb with the dot operator appearing multiple times in the total value.
 - [] Experiment with a system property dedicated to the social medias logic.
-- [] Limit the number of digits the calculator's screen can display at once (between 14 and 16 seems reasonable for this project).
 
 
 
@@ -34,6 +40,10 @@ sign to the very left of the number **if it was transformed into negative value*
 - Learned more about 'this' and how to find its context in a callback function's case.
 - Ended up removing more than writing lines today but i take it as a good thing, i recognized early in the development that my implementation was bad and needed to be rewrote.
 
+### 11/05/2023
+- Wrote the logic for the system keys (github, twitter, close and allClear).
+- Learned about the location property and load method of the Window interface which seems really easy to use, so easy in fact that user's security seems to be a problem but i'll look that up on a bigger project.
+- Set the whole logic for cases where no previous key was registered. 
 
 ## Keys
 **AC** stands for **All Clear**: clears the calculator and resets any functions.
@@ -41,3 +51,5 @@ sign to the very left of the number **if it was transformed into negative value*
 
 ## Sources
 - [Mastering 'this' in JavaScript by Michelle Glenow](https://thenewstack.io/mastering-javascript-callbacks-bind-apply-call/)
+- [Location interface - MDN](https://developer.mozilla.org/en-US/docs/Web/API/Location)
+- [Window:open() method - MDN](https://developer.mozilla.org/en-US/docs/Web/API/Window/open)
--- 11.Calculator/script.js
+++ 11.Calculator/script.js
@@ -48,42 +48,128 @@ function Calculator(screen, keys)
         concatenate(value) { this.value += value; }, 
     };
 
+    this.computation = {
+        leftOperand: {
+            value: null,
+            get() { return this.value },
+            set(newValue) { this.value = newValue },
+        },
+        rightOperand: {
+            value: null,
+            get() { return this.value },
+            set(newValue) { this.value = newValue },
+        },
+        operator: {
+            key: null,
+        },
+        calculate(
+            leftOperand = this.leftOperand,
+            rightOperand = this.rightOperand,
+            operator = this.operator) {
+            switch(operator.key.value) {
+                case "add":
+                    let result = Number(leftOperand) + Number(rightOperand);
+                    return result.toString();
+                    break;
+            }
+        }
+    };
+
     this.system = {
         github(screen) {
-            screen.update('--> GitHub -->');
+            screen.update('Open: github');
+            window.open('https://github.com/joanFaseDev', '_blank', 'noopener');
         },
         twitter(screen) {
-            screen.update('--> Twitter -->');
+            screen.update('Open: twitter');
+            window.open('https://twitter.com/joan_fase', '_blank', 'noopener');
         },
         close(screen) {
-            screen.update('--> Closing... -->');
+            screen.update('Close: application');
             setTimeout(() => {
-                document.querySelector('#calculator').hidden = true;
-                console.dir(document.querySelector('#calculator'));
+                const calculator = document.querySelector('#calculator');
+                calculator.remove();
             }, 2000);
+        },
+        allClear(screen, previousKey, total ) {
+            previousKey.set(null);
+            total.set('0');
+            screen.update(total.get());
+        },
+        load(key, screen, previousKey, total) {
+            (key.value === 'allClear') ? this[key.value](
+                screen,
+                previousKey,
+                total
+                ) : this[key.value](screen);
         }, 
     }
 
     this.handleInputs = function(key) {
         let actualKey = key;
+        // If a key was registered before that one
         if (this.previousKey.get()) {
+            switch (this.previousKey.get().type) {
+                // If that previous key was a digit...
+                case "digit":
+                    // and if the actual one is a digit too
+                    if (actualKey.type === 'digit') {
+                        this.total.concatenate(actualKey.value);
+                        this.screen.update(this.total.get());
+                    }
+                    break;
+                
+                case "system":
+                    this.system.load(
+                        actualkey,
+                        this.screen,
+                        this.previousKey,
+                        this.total
+                    );
+                    break;
 
+                case "operator":
+
+                    break;
+
+                default:
+                    break;
+            }
         } else {
+            // If no previous key was registered
             switch (key.type) {
                 case "digit":
                     this.total.set(key.value);
                     this.screen.update(this.total.value);
                     break;
+
                 case "system":
-                    this.system[key.value](this.screen);
+                    this.system.load(
+                        key,
+                        this.screen,
+                        this.previousKey,
+                        this.total
+                    );
                     break;
+
                 case "operator":
+                    if (key.value === '.') {
+                        this.total.concatenate(key.value);
+                        this.screen.update(this.total.get());
+                    }
+                    break;
+
                 default:
                     break;
             }
+
         }
 
+        this.previousKey.set(key);
+        console.dir(this);
     }
+
+    this.screen.update(this.total.get());
 }
 
 /**
@@ -94,7 +180,13 @@ function Screen(node)
 {
     this.node = node;
     this.update = function(text) {
-        this.node.textContent = text;
+        // Calculator's screen can't handle more than 16 characters.
+        let screenValue = text;
+        if (screenValue.length > 16) {
+            console.log('ALERT -> 16 characters limit!');
+            screenValue = screenValue.substring(0, 16);
+        }
+        this.node.textContent = screenValue;
     };
 }
 
