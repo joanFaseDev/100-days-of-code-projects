1071409732901fa1e576b946523c3153eff597c0
joan_fase <jordanfalaise.dev@gmail.com>
2023-05-17 17:13:09 +0200

    Added a second images carousel demo based on the figma mockup i created for the 100DaysOfCode current project. Experimented more with CSS's position, opacity and z-index properties.

--- Prototypes/ImagesCarousel/script.js
+++ Prototypes/ImagesCarousel/script.js
@@ -1,3 +1,7 @@
+/**
+ * SECTION 1
+ */
+
 let images = Array.from(document.querySelectorAll('.carousel-image'));
 let icons = Array.from(document.querySelectorAll('.icons'));
 let title = document.querySelector('#carousel-title');
@@ -48,6 +52,107 @@ function CarouselManager(images, title) {
         this.updateCarousel(navigation);
     };
 }
+
+/**
+ * SECTION 2
+*/
+
+let imagesCarousel2 = Array.from(document.querySelectorAll(".carousel-2-img"));
+let buttonsCarousel2 = document.querySelectorAll("#carousel-2 #nav .nav-btn");
+let titleCarousel2 = document.querySelector('#carousel-2 #nav #label #title');
+let secondCarousel = new Carousel2(
+    imagesCarousel2, 
+    buttonsCarousel2,
+    titleCarousel2
+);
+
+buttonsCarousel2.forEach((btn) => {
+    btn.addEventListener('click', secondCarousel);
+});
+
+function Carousel2(images, buttons, title) {
+    console.dir(images);
+    console.dir(buttons);
+
+    this.images = images;
+    this.buttons = buttons;
+    this.title = title;
+    this.firstIndex = 0;
+    this.lastIndex = this.images.length - 1;
+    this.imagesPosition = {
+
+        main : this.lastIndex,
+        previous : 3,
+        next : this.firstIndex,
+        first : 2,
+        last : 1,
+    };
+    this.navigationClasses = [
+        'c2-first',
+        'c2-previous',
+        'c2-main',
+        'c2-next',
+        'c2-last'
+    ];
+
+    this.incrementIndex = function(imgsPosition) {
+        for (let img in imgsPosition) {
+            let position = imgsPosition[img];
+            imgsPosition[img] = position + 1;
+
+            if(imgsPosition[img] > this.lastIndex) {
+                imgsPosition[img] = this.firstIndex;
+            }
+        }
+
+    };
+
+    this.decrementIndex = function(imgsPosition) {
+        for (let img in imgsPosition) {
+            let position = imgsPosition[img];
+            imgsPosition[img] = position - 1;
+
+            if(imgsPosition[img] < this.firstIndex) {
+                imgsPosition[img] = this.lastIndex;
+            }
+        };
+    }
+
+    this.resetImagesClass = function() {
+        this.images.forEach((img) => {
+            img.classList.remove(...this.navigationClasses);
+        });
+    };
+
+    this.updateCarousel = function(navigation) {
+        this.resetImagesClass();
+        if (navigation === 'previous') {
+            this.decrementIndex(this.imagesPosition);
+        } else {
+            this.incrementIndex(this.imagesPosition);
+        }
+
+        let main = this.imagesPosition.main;
+        let previous = this.imagesPosition.previous;
+        let next = this.imagesPosition.next;
+        let first = this.imagesPosition.first;
+        let last = this.imagesPosition.last;
+
+        this.images[main].classList.add(this.navigationClasses[2]);
+        this.images[previous].classList.add(this.navigationClasses[1]);
+        this.images[next].classList.add(this.navigationClasses[3]);
+        this.images[first].classList.add(this.navigationClasses[0]);
+        this.images[last].classList.add(this.navigationClasses[4]);
+
+        this.title.textContent = this.images[main].dataset.title;
+    };
+
+    this.handleEvent = function(event) {
+        let navigation = event.target.dataset.navigation;
+        this.updateCarousel(navigation);
+    };
+}
+
 
 
 
